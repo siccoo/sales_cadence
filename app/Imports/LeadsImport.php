c07fe17adb14a523e7d339b0f11b9335781e8a97
inyaJohnson <intoajohnson@gmail.com>
2020-04-25 11:06:04 +0100

    Edit, Delete and Manage Lead

--- app/Imports/LeadsImport.php
+++ app/Imports/LeadsImport.php
@@ -4,7 +4,7 @@ namespace App\Imports;
 
 use App\Lead;
 use Illuminate\Support\Collection;
-//use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -13,13 +13,9 @@ use Maatwebsite\Excel\Concerns\WithValidation;
 
 class LeadsImport implements ToCollection, WithHeadingRow, WithValidation
 {
-
     public function collection(Collection $collection)
     {
-
-
-
-//        $userId = Auth::user()->id;
+//        $userId = Auth::user();
 
         // TODO: Implement collection() method.
         foreach ($collection as $row){
